aa2ac58f35fe7652fc3880a2f580f24337731747
VENU <ajal.krishna.venu@accenture.com>
2026-02-11 21:13:44 +0530

    initial commit with base code setup

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App(){
+    return(
+        <div className="application_wrapper">
+            <h1 style={{textAlign:"center"}}>Quix</h1>
+            <p style={{textAlign:"center"}}>Take a quiz to be become the next quizrr</p>
+        </div>
+    )
+}
